5c7b51f3494ae2cf906b8f2297cd4fd211123854
CongboZangJason <38306608+CongboZangJason@users.noreply.github.com>
2021-10-29 13:20:20 +1100

    Add files via upload

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -4,6 +4,6 @@ vegaEmbed("#bar_chart", vg_1).then(function(result) {
 }).catch(console.error);
 
 var vg_2 = "symbol_map.vg.json";
-vegaEmbed("symbol_map", vg_2).then(function(result) {
+vegaEmbed("#symbol_map", vg_2).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
